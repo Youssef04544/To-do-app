ae05f64342703d10b87cc63dddccd567093b8de0
Youssef Ben Yedder <youssefbenyedder04@gmail.com>
2023-10-21 15:52:12 -0400

    Added more css and container to style it
    Still needs a lot of work for styling

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,17 @@ function App() {
     setTodos(oldtodos => oldtodos.filter((todo)=> todo.id !== id))
   }
   return (
-    <>
+    <div className='mx-[25%] my-[5%] bg-yellow-400 flex flex-col items-center'>
+   {/*gotta keep updating the css to center the form and give it the right size and make sure the todolist is well adjusted*/}
     <h1>This is a todo list web application</h1>
-    <form onSubmit={(e)=>addTodo(e)}>
-      <input className='border-2' type="text" value={task} onChange={e=>setTask(e.target.value)} />
-      <input type="submit" value="Add todo" />
+    <form className='my-2 w-full' onSubmit={(e)=>addTodo(e)}>
+      <input className='border-2 w-1/2' type="text" value={task} onChange={e=>setTask(e.target.value)} />
+      <input className='ml-4' type="submit" value="Add todo" />
     </form>
     {todos.length === 0 && <p>Nothing to do yet.</p>}
-    <TodoList handleDelete={handleDelete} todos={todos}/>
-    </>
+    <TodoList handleDelete={handleDelete} todos={todos}/>    
+
+    </div>
   );
 }
 
