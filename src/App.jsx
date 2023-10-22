32b523168c0b84b4ecb5e7b70d59f138f3e5086b
Youssef Ben Yedder <youssefbenyedder04@gmail.com>
2023-10-22 06:42:57 -0400

    added some more styling modifications
    Found some designs to follow for future styling

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,12 @@ function App() {
     setTodos(oldtodos => oldtodos.filter((todo)=> todo.id !== id))
   }
   return (
-    <div className='mx-[25%] my-[5%] bg-yellow-400 flex flex-col items-center'>
+    <div className='mx-[25%] my-[5%] p-4 h-[80vh] bg-gray-400 '>
    {/*gotta keep updating the css to center the form and give it the right size and make sure the todolist is well adjusted*/}
-    <h1>This is a todo list web application</h1>
-    <form className='my-2 w-full' onSubmit={(e)=>addTodo(e)}>
-      <input className='border-2 w-1/2' type="text" value={task} onChange={e=>setTask(e.target.value)} />
-      <input className='ml-4' type="submit" value="Add todo" />
+    <h1>To-do's</h1>
+    <form className='my-2 w-full ' onSubmit={(e)=>addTodo(e)}>
+      <input className='border-2 w-1/2 px-1' type="text" value={task} onChange={e=>setTask(e.target.value)} />
+      <input className='ml-4 px-4 cursor-pointer bg-gray-300' type="submit" value="Add" />
     </form>
     {todos.length === 0 && <p>Nothing to do yet.</p>}
     <TodoList handleDelete={handleDelete} todos={todos}/>    
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const TodoList = ({handleDelete, todos}) => {
   return (
-    <ul>
+    <ul className='mt-16 w-full'>
       {todos.map((item)=>{
         return <div key={item.id}>
         
-      <li>{item.todo} {item.id}</li>
-      <input type="button" value="Delete" onClick={()=>handleDelete(item.id)}/>
+      <li className='inline bg-white px-2 py-1'>{item.todo} {item.id}</li>
+      <input className='ml-4 px-2 py-1 cursor-pointer bg-gray-300' type="button" value="Delete" onClick={()=>handleDelete(item.id)}/>
       </div>}
       )}
     </ul>
