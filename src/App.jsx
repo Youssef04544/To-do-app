596996c191f7157cbd7ee283685266dc236c957a
Youssef Ben Yedder <youssefbenyedder04@gmail.com>
2023-10-25 10:52:35 -0400

    Made the app look a lot more like the desired design
    a few more adjusments left before going back to working on functionnality

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,13 @@ function App() {
     setTodos(oldtodos => oldtodos.filter((todo)=> todo.id !== id))
   }
   return (
-    <div className='mx-[25%] my-[5%] p-4 h-[80vh] bg-gray-400 '>
+    <div className='mx-[35%] my-[5%] h-[80vh] bg-white'>
    {/*gotta keep updating the css to center the form and give it the right size and make sure the todolist is well adjusted*/}
-    <h1>To-do's</h1>
-    <form className='my-2 w-full ' onSubmit={(e)=>addTodo(e)}>
-      <input className='border-2 w-1/2 px-1' type="text" value={task} onChange={e=>setTask(e.target.value)} />
-      <input className='ml-4 px-4 cursor-pointer bg-gray-300' type="submit" value="Add" />
+    <h1 className='text-center font-bold text-3xl p-3'>TODO LIST</h1>
+    <form className='my-2 w-full flex justify-center p-4' onSubmit={(e)=>addTodo(e)}>
+      <input className='border-2 w-full p-2 rounded-md' type="text" placeholder='add a new task...' value={task} onChange={e=>setTask(e.target.value)} />
+      {/*Make the button position absolute and size it and color it correctly*/}
+      <input className='ml-4 p-2 cursor-pointer rounded-full text-xl center bg-gray-300' type="submit" value="+" />
     </form>
     {todos.length === 0 && <p>Nothing to do yet.</p>}
     <TodoList handleDelete={handleDelete} todos={todos}/>    
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,10 +4,10 @@ const TodoList = ({handleDelete, todos}) => {
   return (
     <ul className='mt-16 w-full'>
       {todos.map((item)=>{
-        return <div key={item.id}>
+        return <div key={item.id} className='w-full p-2 border-2 border-gray-200 rounded-md'>
         
-      <li className='inline bg-white px-2 py-1'>{item.todo} {item.id}</li>
-      <input className='ml-4 px-2 py-1 cursor-pointer bg-gray-300' type="button" value="Delete" onClick={()=>handleDelete(item.id)}/>
+      <li className='inline px-2 py-1'>{item.todo} {item.id}</li>
+      <input className='ml-4 px-2 py-1 cursor-pointer' type="button" value="X" onClick={()=>handleDelete(item.id)}/>
       </div>}
       )}
     </ul>
