1d60ce59ce25935056b1db12defc6b164c60b57c
Youssef Ben Yedder <youssefbenyedder04@gmail.com>
2023-10-28 06:01:17 -0400

    Adjusted the add button position
    Tried fontawesome, decided to go with regular plus sign, discovered a few things I need to fix like stretching the list or make it scrollable and validate form before submitting

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   const [task, setTask] = useState('');
   const [todos, setTodos] = useState([]);
-  const [isEditing, setIsEditing] = useState({editing:false})
+  const [isEditing, setIsEditing] = useState({ editing: false })
   const inputRef = useRef();
   const dragItem = useRef();
   const dragOverItem = useRef();
@@ -15,17 +15,17 @@ function App() {
   const addTodo = (e) => {
     e.preventDefault()
     //Checks if we're editing a todo
-    if (!isEditing.editing){
-      setTodos(oldtodos =>{
+    if (!isEditing.editing) {
+      setTodos(oldtodos => {
         setTask("");
-        return [...oldtodos, {task,id: globalID++, completed:false}]
+        return [...oldtodos, { task, id: globalID++, completed: false }]
       })
-    }else {
-      setTodos(oldtodos=> {
+    } else {
+      setTodos(oldtodos => {
         setTask('');
-        setIsEditing({editing:false})
+        setIsEditing({ editing: false })
         return oldtodos.map(todo => {
-          if (todo.id === isEditing.todo.id) return {...todo, task}
+          if (todo.id === isEditing.todo.id) return { ...todo, task }
           return todo
         })
       })
@@ -33,26 +33,26 @@ function App() {
     inputRef.current?.focus();
   }
   const handleDelete = (id) => {
-    setTodos(oldtodos => oldtodos.filter((todo)=> todo.id !== id))
+    setTodos(oldtodos => oldtodos.filter((todo) => todo.id !== id))
   }
 
   const handleCompleted = (id) => {
-    setTodos (oldtodos => oldtodos.map((todo)=>{
-      if(todo.id === id) return {...todo, completed: !todo.completed}
+    setTodos(oldtodos => oldtodos.map((todo) => {
+      if (todo.id === id) return { ...todo, completed: !todo.completed }
       return todo
     }))
   }
 
   const handleEdit = (todo) => {
     setTask(todo.task)
-    setIsEditing({todo, editing:true})
+    setIsEditing({ todo, editing: true })
   }
 
-  const handleDragStart = (e,position) => {
+  const handleDragStart = (e, position) => {
     dragItem.current = position;
   }
 
-  const handleDragEnter = (e,position) => {
+  const handleDragEnter = (e, position) => {
     dragOverItem.current = position;
   }
 
@@ -61,24 +61,24 @@ function App() {
     const copyTodos = [...todos];
     const dragItemContent = copyTodos[dragItem.current];
     copyTodos.splice(dragItem.current, 1);
-    copyTodos.splice(dragOverItem.current,0, dragItemContent)
+    copyTodos.splice(dragOverItem.current, 0, dragItemContent)
     dragItem.current = null;
     dragOverItem.current = null;
     setTodos(copyTodos);
   }
-
+  //Put a validation on the form input if its empty dont pass an empty task and solve the tasklist size if i add too
+  //many tasks it gets out of the border and gets messy
   return (
     <div className='mx-[35%] my-[5%] h-[80vh] bg-white rounded-lg'>
-   {/*gotta keep updating the css to center the form and give it the right size and make sure the todolist is well adjusted*/}
-    <h1 className='text-center font-bold text-3xl p-3'>TODO LIST</h1>
-    <form className='my-2 w-full flex justify-center p-4' onSubmit={(e)=>addTodo(e)}>
-      <input className='border-2 w-full p-2 rounded-md'autoFocus ref={inputRef} type="text" placeholder='add a new task...' value={task} onChange={e=>setTask(e.target.value)} />
-      {/*Make the button position absolute and size it and color it correctly*/}
-      <input className='ml-4 p-2 cursor-pointer rounded-full text-xl center bg-gray-300' type="submit" value="+" />
-    </form>
-    {todos.length === 0 && <p className='mx-2'>Nothing to do yet.</p>}
-    <TodoList handleDelete={handleDelete} handleEdit={handleEdit} handleCompleted={handleCompleted} 
-    handleDragStart={handleDragStart} todos={todos} handleDragEnter={handleDragEnter} handleDrop={handleDrop}/>    
+      <h1 className='text-center font-bold text-3xl p-6'>TODO LIST</h1>
+      <form className=' w-full flex justify-center p-4 relative' onSubmit={(e) => addTodo(e)}>
+        <input className='border-2 w-2/3 p-2 rounded-md' autoFocus ref={inputRef} type="text" placeholder='add a new task...' value={task} onChange={e => setTask(e.target.value)} />
+        {/*Make the button position absolute and size it and color it correctly*/}
+        <input className='ml-4 p-2 h-10 w-10 flex justify-center cursor-pointer rounded-full text-xl bg-gray-300 absolute top-4 right-[-15px]' type="submit" value="+" />
+      </form>
+      {todos.length === 0 && <p className='mx-2'>Nothing to do yet.</p>}
+      <TodoList handleDelete={handleDelete} handleEdit={handleEdit} handleCompleted={handleCompleted}
+        handleDragStart={handleDragStart} todos={todos} handleDragEnter={handleDragEnter} handleDrop={handleDrop} />
 
     </div>
   );
